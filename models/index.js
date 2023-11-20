7f816b1314c52dbbed9ac5ec34c3925af1455892
toum21 <pey2moo1@gmail.com>
2023-11-20 15:12:14 -0800

    updated index.js that is inside of the models folder

--- models/index.js
+++ models/index.js
@@ -2,11 +2,33 @@ const User = require('./User');
 const Comment = require('./Comment');
 const BlogPost = require('./BlogPost');
 
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id'
+});
+
+BlogPost.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+BlogPost.hasMany(Comment, {
+    foreignKey: 'blog_post_id'
+})
+
+Comment.belongsTo(BlogPost, {
+    foreignKey: 'blog_post_id'
+})
 
 
 
 
 
 
-
-module.exports = {User, Comment, BlogPost}
+module.exports = { User, Comment, BlogPost }
