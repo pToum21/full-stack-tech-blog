5876db6010293fa76d3047f88ed6c123cb0e8e70
toum21 <pey2moo1@gmail.com>
2023-11-20 15:32:40 -0800

    created seeds for blogposts

---
+++ seeds/blogPost-seeds.js
@@ -0,0 +1,26 @@
+const { BlogPost } = require('../models')
+
+const blogPostData = [
+    {
+        title: 'new gtx 4090 is amazing',
+        description: 'it may just be the best graphics card ever',
+        upload_date: 'March 17, 2023',
+        user_id: 1
+    },
+    {
+        title: 'I cant choose what cpu to buy',
+        description: 'can someone please help me pick what new cpu to upgrade to',
+        upload_date: 'December 1, 2023',
+        user_id: 2
+    },
+    {
+        title: 'IDK im bored and wanted to see whats up',
+        description: 'someone tell me whats the move im gaming right now',
+        upload_date: 'june 26, 2024',
+        user_id: 3
+    },
+];
+
+const seedBlogPost = () => Product.bulkCreate(productData);
+
+module.exports = seedBlogPost;
--- seeds/index.js
+++ seeds/index.js
@@ -17,4 +17,6 @@ const seedAll = async () => {
     process.exit(0)
 };
 
+
+
 seedAll();
