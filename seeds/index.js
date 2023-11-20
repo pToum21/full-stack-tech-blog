e4bbbae48d137e1320967125256d81445b27e2c8
toum21 <pey2moo1@gmail.com>
2023-11-20 15:20:58 -0800

    updated the index.js inside of seeds folder

---
+++ seeds/index.js
@@ -0,0 +1,20 @@
+const seedBlogPost = require('./blogPost-seeds')
+const seedComment = require('./comment-seeds')
+const seedUser = require('./user-seeds')
+
+const sequelize = require('../config/connection')
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await seedUser();
+    console.log('\n----- USERS SEEDED -----\n');
+    await seedBlogPost();
+    console.log('\n----- BLOG POSTS SEEDED -----\n');
+    await seedComment();
+    console.log('\n----- COMMENTS SEEDED -----\n');
+
+    process.exit(0)
+};
+
+seedAll();
