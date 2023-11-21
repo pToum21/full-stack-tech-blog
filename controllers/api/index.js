73952987d740fa69416bda77f311c5447f3ced91
toum21 <pey2moo1@gmail.com>
2023-11-21 11:26:41 -0800

    updated index.js inside of controllers api

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes')
+const blogPostRoutes = require('./blogPostRoutes')
+const commentRoutes = require('./commentRoutes')
+
+router.use('/users', userRoutes);
+router.use('/post', blogPostRoutes);
+router.use('/comment', commentRoutes);
+
+module.exports = router;
+
+
