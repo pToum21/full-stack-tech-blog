95505cc269b5d74098f46dc7dc26a4d33493ad8d
toum21 <pey2moo1@gmail.com>
2023-11-20 18:22:33 -0800

    createrd an index.js for controllers

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const homeRoutes = require('./html/homeroutes')
+const dashboardRoutes = require('./html/dashboard')
+
+const apiRoutes = require('./api')
+
+router.use('/', homeRoutes);
+
+router.use('/api', apiRoutes);
+
+router.use('/dashboard', dashboardRoutes);
+
+module.exports = router;
