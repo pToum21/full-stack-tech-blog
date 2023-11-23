7dce1a84ac556f1b37713073a44495c4a17227d6
toum21 <pey2moo1@gmail.com>
2023-11-22 17:31:13 -0800

    added basic styling in and loginpage.handlebars as well as updated main.handlebars

--- controllers/html/homeroutes.js
+++ controllers/html/homeroutes.js
@@ -24,5 +24,15 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('login');
+  });
+
 
 module.exports = router;
